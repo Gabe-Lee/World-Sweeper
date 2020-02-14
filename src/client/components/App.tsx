6110141ab885a4ae3dca19871e4f9a5d6055e6c9
Gabriel Anderson <gla.ander@gmail.com>
2020-02-14 12:34:59 -0600

    [WS-50] Add basic theme provider for app

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { dark, light } from './style/themes';
 
 const App = (): JSX.Element => {
-  return <div id="app">HELLO WORLD</div>;
+  const prefersDarkTheme = useMediaQuery('(prefers-color-scheme: dark)');
+
+  return (
+    <ThemeProvider theme={prefersDarkTheme ? dark : light}>
+      <div id="app">HELLO WORLD</div>
+    </ThemeProvider>
+  );
 };
 export default App;
