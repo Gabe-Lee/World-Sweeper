a8672ce56d7eb9823fad51d93188cc7908e8529c
Gabriel Anderson <gla.ander@gmail.com>
2020-02-14 20:59:52 -0600

    [WS-51] Create adaptive flex grid styles

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,6 +1,18 @@
 import React, { ReactElement } from 'react';
 
+import './normalize.scss';
+import './flex.scss';
+
 const App = (): ReactElement => {
-  return <div id="app">HELLO WORLD</div>;
+  return (
+    <>
+      <div id="app">HELLO WORLD</div>
+      <div className="grid space-1">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => (
+          <div className="flex-xs-1 flex-md-2 flex-lg-4">{num}</div>
+        ))}
+      </div>
+    </>
+  );
 };
 export default App;
